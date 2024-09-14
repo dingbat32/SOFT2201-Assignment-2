6002856993d2b68b922f56e55c6b0e08d731c2c9
dingbat32 <sven.willett@gmail.com>
2024-09-14 13:52:04 +1000

    Implement WallFactory

--- src/main/java/pacman/model/factory/WallFactory.java
+++ src/main/java/pacman/model/factory/WallFactory.java
@@ -1,7 +1,12 @@
 package pacman.model.factory;
 
+import javafx.scene.image.Image;
+import pacman.model.engine.ImageLoader;
 import pacman.model.entity.Renderable;
+import pacman.model.entity.dynamic.physics.BoundingBox;
+import pacman.model.entity.dynamic.physics.BoundingBoxImpl;
 import pacman.model.entity.dynamic.physics.Vector2D;
+import pacman.model.entity.staticentity.StaticEntityImpl;
 
 public class WallFactory implements RenderableFactory {
     private int wallType;
@@ -10,6 +15,31 @@ public class WallFactory implements RenderableFactory {
     }
     @Override
     public Renderable createRenderable(Vector2D position) {
-        return null;
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        Image image;
+        switch (wallType) {
+            case 1:
+                image = imageLoader.loadImage("walls/horizontal.png");
+                break;
+            case 2:
+                image = imageLoader.loadImage("walls/vertical.png");
+                break;
+            case 3:
+                image = imageLoader.loadImage("walls/upLeft.png");
+                break;
+            case 4:
+                image = imageLoader.loadImage("walls/upRight.png");
+                break;
+            case 5:
+                image = imageLoader.loadImage("walls/downRight.png");
+                break;
+            case 6:
+                image = imageLoader.loadImage("walls/downLeft.png");
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid wall type");
+        }
+        BoundingBox boundingBox = new BoundingBoxImpl(position, image.getHeight(), image.getWidth());
+        return new StaticEntityImpl(boundingBox, Renderable.Layer.FOREGROUND, image);
     }
 }
