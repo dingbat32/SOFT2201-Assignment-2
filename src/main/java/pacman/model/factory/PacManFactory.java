df0ffc1b710040d9bc363b67ea512fef6a45d689
dingbat32 <sven.willett@gmail.com>
2024-09-14 13:39:24 +1000

    Add pellet factory

--- src/main/java/pacman/model/factory/PacManFactory.java
+++ src/main/java/pacman/model/factory/PacManFactory.java
@@ -15,17 +15,17 @@ public class PacManFactory implements RenderableFactory {
     @Override
     public Renderable createRenderable(Vector2D position) {
         ImageLoader imageLoader = ImageLoader.getInstance();
-        Image ghostImage = imageLoader.loadImage("ghosts/ghost.png");
         Map<PacmanVisual, Image> pacmanVisualImages = new HashMap<>();
         pacmanVisualImages.put(PacmanVisual.CLOSED, imageLoader.loadImage("pacman/playerClosed.png"));
         pacmanVisualImages.put(PacmanVisual.DOWN, imageLoader.loadImage("pacman/playerDown.png"));
         pacmanVisualImages.put(PacmanVisual.LEFT, imageLoader.loadImage("pacman/playerLeft.png"));
         pacmanVisualImages.put(PacmanVisual.RIGHT, imageLoader.loadImage("pacman/playerRight.png"));
         pacmanVisualImages.put(PacmanVisual.UP, imageLoader.loadImage("pacman/playerUp.png"));
-        BoundingBox pacmanBox = new BoundingBoxImpl(position, ghostImage.getHeight(), ghostImage.getWidth());
+        Image closedImage = pacmanVisualImages.get(PacmanVisual.CLOSED);
+        BoundingBox pacmanBox = new BoundingBoxImpl(position, closedImage.getHeight(), closedImage.getWidth());
         KinematicStateImpl state = new KinematicStateImpl.KinematicStateBuilder()
                 .setPosition(position)
                 .build();
-        return new Pacman(pacmanVisualImages.get(PacmanVisual.CLOSED), pacmanVisualImages, pacmanBox, state);
+        return new Pacman(closedImage, pacmanVisualImages, pacmanBox, state);
     }
 }
--- src/main/java/pacman/model/factory/PelletFactory.java
+++ src/main/java/pacman/model/factory/PelletFactory.java
@@ -1,12 +1,20 @@
 package pacman.model.factory;
 
+import javafx.scene.image.Image;
+import pacman.model.engine.ImageLoader;
 import pacman.model.entity.Renderable;
+import pacman.model.entity.dynamic.physics.BoundingBox;
+import pacman.model.entity.dynamic.physics.BoundingBoxImpl;
 import pacman.model.entity.dynamic.physics.Vector2D;
+import pacman.model.entity.staticentity.collectable.Pellet;
 
 public class PelletFactory implements RenderableFactory {
 
     @Override
     public Renderable createRenderable(Vector2D position) {
-        return null;
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        Image image = imageLoader.loadImage("pellet.png");
+        BoundingBox boundingBox = new BoundingBoxImpl(position, image.getHeight(), image.getWidth());
+        return new Pellet(boundingBox, Renderable.Layer.FOREGROUND, image, 100);
     }
 }
