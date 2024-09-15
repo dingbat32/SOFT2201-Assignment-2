6d45eac29f4ae64fbeff89e36ededef67586d33a
dingbat32 <sven.willett@gmail.com>
2024-09-15 11:14:01 +1000

    Add movement to pacman

--- src/main/java/pacman/view/keyboard/KeyboardInputHandler.java
+++ src/main/java/pacman/view/keyboard/KeyboardInputHandler.java
@@ -2,28 +2,41 @@ package pacman.view.keyboard;
 
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import pacman.model.entity.Renderable;
+import pacman.model.entity.dynamic.player.Controllable;
+import pacman.model.entity.dynamic.player.Pacman;
+import pacman.model.maze.Maze;
 
 /**
  * Responsible for handling keyboard input from player
  */
 public class KeyboardInputHandler {
-
+    Controllable controllable;
 
     public KeyboardInputHandler() {
-
+        Renderable givenControllable = Maze.getInstance().getControllable();
+        if (givenControllable instanceof Controllable) {
+            controllable = (Controllable) givenControllable;
+        } else {
+            throw new RuntimeException("getControllable() did not get controllable");
+        }
     }
 
     public void handlePressed(KeyEvent keyEvent) {
         KeyCode keyCode = keyEvent.getCode();
         switch (keyCode) {
             case LEFT:
+                controllable.left();
                 break;
             case RIGHT:
+                controllable.right();
                 break;
             case DOWN:
+                controllable.down();
                 break;
             case UP:
+                controllable.up();
                 break;
-        };
+        }
     }
 }
